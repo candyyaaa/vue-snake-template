0bb92a270e87c91fee38ccc2571a91046f6c64f8
candy <littlecandyi@163.com>
2023-02-04 22:20:37 +0800

    feat：新增 echarts 组件

--- src/components/charts-box/index.vue
+++ src/components/charts-box/index.vue
@@ -3,13 +3,22 @@
  * @Author: candy littlecandyi@163.com
  * @Date: 2023-02-04 00:22:36
  * @LastEditors: candy littlecandyi@163.com
- * @LastEditTime: 2023-02-04 00:23:47
+ * @LastEditTime: 2023-02-04 22:19:55
 -->
 <template>
   <div class="charts-wrap"><h1>charts</h1></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import 'echarts-liquidfill'
+import * as echarts from 'echarts'
+import { createUuId } from '@/utils'
+
+interface Props {
+  uuId: string | number
+  options: any
+}
+</script>
 
 <style lang="scss" scoped>
 .charts-wrap {
